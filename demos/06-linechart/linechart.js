8fcfa70ed4db88c3d7e9c5ce04521656fc003b8b
Victor Mejia <victor.a.mejia@gmail.com>
2014-04-30 15:05:20 -0700

    Added transition to line chart stock demo

--- demos/06-linechart/linechart.js
+++ demos/06-linechart/linechart.js
@@ -28,7 +28,7 @@ var mouseTarget = svg.append("rect")
 var parseDate = d3.time.format("%d-%b-%y").parse;
 	bisectDate = d3.bisector(function(d) { return d.date; }).left,
 	formatValue = d3.format(",.2f"),
-    formatCurrency = function(d) { return "$" + formatValue(d); };
+	formatCurrency = function(d) { return "$" + formatValue(d); };
 
 var x = d3.time.scale()
 	.range([0, width]);
@@ -41,7 +41,7 @@ var xAxis = d3.svg.axis()
 	.orient("bottom")
 	.ticks(d3.time.months, 1)
 	.tickPadding(7)
-   	.tickFormat(d3.time.format("%b"));;
+	.tickFormat(d3.time.format("%b"));;
 
 var yAxis = d3.svg.axis()
 	.scale(y)
@@ -64,11 +64,13 @@ d3.csv("aapl.csv", function(error, data) {
 	x.domain([data[0].date, data[data.length - 1].date]);
 	y.domain(d3.extent(data, function(d) { return d.close; }));
 
+	// x-axis
 	svg.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis);
 
+	// y-axis	
 	svg.append("g")
 			.attr("class", "y axis")
 			.call(yAxis)
@@ -79,12 +81,37 @@ d3.csv("aapl.csv", function(error, data) {
 			.style("text-anchor", "end")
 			.text("Price ($)");
 
-	svg.append("path")
+	// stock line chart
+	var path = svg.append("path")
 			.datum(data)
 			.attr("class", "line")
-			.attr("d", line)
+			.attr("d", line);
 
+	// animate path
+	// src: http://bl.ocks.org/duopixel/4063326
+	var totalLength = path.node().getTotalLength();
+	path.attr("stroke-dasharray", totalLength + " " + totalLength)
+  		.attr("stroke-dashoffset", totalLength)
+  		.transition()
+			.duration(700)
+			.ease("linear")
+			.attr("stroke-dashoffset", 0);
+
+	// tooltip group
+	var tooltip = svg.append('g').attr('class', 'linechart-tooltip').style('visibility', 'hidden'),
+		tooltipWidth = 130,
+		tooltipHeight = 40;
+	tooltip.append('line');
+	tooltip.append('circle').attr({ r: 3, class: 'inner-circle', 'pointer-events': 'none' });
+	tooltip.append('circle').attr({ r: 10, class: 'outer-circle', 'pointer-events': 'none' });
+	tooltip.append('rect').attr({ width: tooltipWidth, height: tooltipHeight, 'pointer-events': 'none' });	
+
+	var text = tooltip.append('text').attr('pointer-events', 'none')
+	
 	mouseTarget.on('mousemove', showTooltip);
+	svg.on('mouseleave', function () {
+		tooltip.style('visibility', 'hidden')
+	});
 
 	function showTooltip() {
 		var coords = d3.mouse(this),
@@ -93,28 +120,72 @@ d3.csv("aapl.csv", function(error, data) {
 			var d0 = data[i - 1],
 			d1 = data[i];
 			d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-			// focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-			// focus.select("text").text(formatCurrency(d.close));
 
-		svg.select('.linechart-tooltip').remove();
+		// the values
+		var xPos = coords[0];
+		var yPos = y(d.close);
 
-		var tooltip = svg.append('g').attr('class', 'linechart-tooltip');
+		var tooltip = svg.select('.linechart-tooltip');
 
-		tooltip.append("line")
-			.attr({
-				x1: coords[0], y1: height, x2: coords[0], y2: 0
-			});
+		tooltip.select("line").attr({ x1: xPos, y1: height, x2: xPos, y2: 0 });
 
-		tooltip.append('circle')
-				.attr('cx', coords[0])
-				.attr('cy', y(d.close))
-				.attr('r', 3)
-				.attr('class', 'inner-circle');
+		tooltip.select('.inner-circle').attr('cx', xPos).attr('cy', yPos)
 
-		tooltip.append('circle')
-				.attr('cx', coords[0])
-				.attr('cy', y(d.close))
-				.attr('r', 10)
-				.attr('class', 'outer-circle');
-	}			
+		tooltip.select('.outer-circle').attr('cx', xPos).attr('cy', yPos)
+
+		var tooltipPos = getTooltipPos({x: xPos, y: yPos});
+
+		tooltip.select('rect').attr({ x: tooltipPos.x, y: tooltipPos.y});
+
+		var text = tooltip.select('text');
+		text.selectAll('tspan').remove();
+
+		text.attr('x', tooltipPos.x + 10).attr('y', tooltipPos.y + 15);
+
+		text.append('tspan')
+			.attr('text-anchor', 'start')
+			.text(function () {
+				return d3.time.format('%a, %b %d %Y')(d.date);
+			})
+		text.append('tspan')
+			.attr('dy', 16)
+			.attr('dx', -96)
+			.attr('text-anchor', 'start')
+			.text(function () {
+				return 'Price: ' + formatCurrency(d.close);
+			})
+
+		tooltip.style('visibility', 'visible');
+	}
+
+	function getTooltipPos(pos) {
+		// check right boundary
+		if ((pos.x + Math.round(tooltipWidth / 2)) > width) {
+			return {
+				x: pos.x - tooltipWidth - 20,
+				y: pos.y - Math.round(tooltipHeight / 2)
+			};
+		}
+
+		// check top boundary 
+		if ((pos.y - tooltipHeight) < 0 ) {
+			return {
+				x: pos.x - Math.round(tooltipWidth / 2),
+				y: pos.y + Math.round(tooltipHeight / 2)
+			};
+		}
+
+		// check left boundary
+		if ((pos.x - Math.round(tooltipWidth / 2)) < 0) {
+			return {
+				x: pos.x + 20,
+				y: pos.y - Math.round(tooltipHeight / 2)
+			}
+		}
+
+		return {
+			x: pos.x - Math.round(tooltipWidth / 2),
+			y: pos.y - tooltipHeight - 20
+		};
+	}		
 });
