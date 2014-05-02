52d9df6b71ed0cf073295ee92b9e82e12633fb6b
Victor Mejia <victor.a.mejia@gmail.com>
2014-05-02 16:33:34 -0700

    Cleaned up demos

--- demos/02-barchart-axis/barchart.js
+++ demos/02-barchart-axis/barchart.js
@@ -1,70 +1,70 @@
 var el = d3.select('#chart'),
-	margin = {top: 40, right: 10, bottom: 80, left: 40}, // margin used for axis
-	elWidth = parseInt(el.style('width'), 10),
-	elHeight = parseInt(el.style('height'), 10),
-	width = elWidth - margin.right - margin.left,
-	height = elHeight - margin.top - margin.bottom,
-	barWidth = Math.round(width / data.length),
-	fillColor = '#2d323d';
+  margin = {top: 40, right: 10, bottom: 80, left: 40}, // margin used for axis
+  elWidth = parseInt(el.style('width'), 10),
+  elHeight = parseInt(el.style('height'), 10),
+  width = elWidth - margin.right - margin.left,
+  height = elHeight - margin.top - margin.bottom,
+  barWidth = Math.round(width / data.length),
+  fillColor = '#2d323d';
 
 var svg = d3.select("#chart").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 // we need to scale
 var yScale = d3.scale.linear()
-	.domain([0, d3.max(data, function (d) { return d.value})])
-	.range([height, 0])
+  .domain([0, d3.max(data, function (d) { return d.value})])
+  .range([height, 0])
 
 var xScale = d3.scale.ordinal()
-	.domain(data.map(function (d) { return d.lang; }))
-	.rangeRoundBands([0, width], 0.1);
+  .domain(data.map(function (d) { return d.lang; }))
+  .rangeRoundBands([0, width], 0.1);
 
 // create axis
 var xAxis = d3.svg.axis()
-	.scale(xScale)
-	.orient("bottom") // place label below tick
-	.tickPadding(10); // padding b/n tick and label
+  .scale(xScale)
+  .orient("bottom") // place label below tick
+  .tickPadding(10); // padding b/n tick and label
 
 var yAxis = d3.svg.axis()
-	.scale(yScale)
-	.orient("left")
-	.tickPadding(10)
-	.tickFormat(d3.format("s"));
+  .scale(yScale)
+  .orient("left")
+  .tickPadding(10)
+  .tickFormat(d3.format("s"));
 
 var xAxisGroup = svg.append("g").attr({
-	class : 'axis',
-	transform: 'translate(' + [0, height] + ')'
+  class : 'axis',
+  transform: 'translate(' + [0, height] + ')'
 }).call(xAxis)
-	.selectAll("text")
-		.style("text-anchor", "end")
-		.attr("dx", "-10px")
-		.attr("dy", "-11px")
-		.attr({
-			transform: 'rotate(-90)'
-		})
+  .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-10px")
+    .attr("dy", "-11px")
+    .attr({
+      transform: 'rotate(-90)'
+    })
 
 var yAxisGroup = svg.append('g').attr({
-	class: 'axis'
+  class: 'axis'
 }).call(yAxis);
 
 // create visualization
 var r = svg.selectAll('rect')
-	.data(data)
-	.enter()
-	.append('rect')
-	.attr({
-		x: function (d, i) {
-			return xScale(d.lang)
-		},
-		y: function (d, i) {
-			return yScale(d.value);
-		},
-		height: function (d) {
-			return height - yScale(d.value);
-		},
-		width: xScale.rangeBand(),
-		fill: fillColor
-	})
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr({
+    x: function (d, i) {
+      return xScale(d.lang)
+    },
+    y: function (d, i) {
+      return yScale(d.value);
+    },
+    height: function (d) {
+      return height - yScale(d.value);
+    },
+    width: xScale.rangeBand(),
+    fill: fillColor
+  })
--- demos/barchart.data.js
+++ demos/barchart.data.js
@@ -1,46 +1,45 @@
 var data = [
-	{ lang: 'JavaScript', value: 549385},
-	{ lang: 'Ruby', value: 453004},
-	{ lang: 'Java', value: 375857},
-	{ lang: 'PHP', value: 278937},
-	{ lang: 'Python', value: 247099},
-	{ lang: 'C++', value: 177001},
-	{ lang: 'C', value: 167175},
-	{ lang: 'CSS', value: 105897},
-	{ lang: 'C#', value: 76874},
-	{ lang: 'Objective-C', value: 75399},
-	{ lang: 'Shell', value: 70516},
-	{ lang: 'Perl', value: 47954},
-	{ lang: 'CoffeeScript', value: 27402},
-	{ lang: 'Go', value: 23334}
-	
+  { lang: 'JavaScript', value: 549385},
+  { lang: 'Ruby', value: 453004},
+  { lang: 'Java', value: 375857},
+  { lang: 'PHP', value: 278937},
+  { lang: 'Python', value: 247099},
+  { lang: 'C++', value: 177001},
+  { lang: 'C', value: 167175},
+  { lang: 'CSS', value: 105897},
+  { lang: 'C#', value: 76874},
+  { lang: 'Objective-C', value: 75399},
+  { lang: 'Shell', value: 70516},
+  { lang: 'Perl', value: 47954},
+  { lang: 'CoffeeScript', value: 27402},
+  { lang: 'Go', value: 23334} 
 ];
 
 //d3.shuffle(data);
 
 var data2 = [
-	{ 
-		year: '2012',
-		values: [
-			{ lang: "Ruby", value: 494188},
-			{ lang: "Java", value: 419527},
-			{ lang: "JavaScript", value: 415401}
-		]
-	},
-	{
-		year: '2013',
-		values: [
-			{ lang: "Ruby", value: 453004},
-			{ lang: "Java", value: 375857},
-			{ lang: "JavaScript", value: 549385}
-		]
-	},
-	{
-		year: '2014',
-		values: [
-			{ lang: "Ruby", value: 186982},
-			{ lang: "Java", value: 203716},
-			{ lang: "JavaScript", value: 281472}
-		]
-	},
+  { 
+    year: '2012',
+    values: [
+      { lang: "Ruby", value: 494188},
+      { lang: "Java", value: 419527},
+      { lang: "JavaScript", value: 415401}
+    ]
+  },
+  {
+    year: '2013',
+    values: [
+      { lang: "Ruby", value: 453004},
+      { lang: "Java", value: 375857},
+      { lang: "JavaScript", value: 549385}
+    ]
+  },
+  {
+    year: '2014',
+    values: [
+      { lang: "Ruby", value: 186982},
+      { lang: "Java", value: 203716},
+      { lang: "JavaScript", value: 281472}
+    ]
+  },
 ];
