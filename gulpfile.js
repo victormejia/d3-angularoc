767bad3c61283a3b7a28c2b9ab16bef273341ba8
Victor Mejia <victor.a.mejia@gmail.com>
2014-05-05 14:03:10 -0700

    Added deploy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,51 +4,48 @@ var gulp = require('gulp');
 
 // load plugins
 var $ = require('gulp-load-plugins')();
+var deploy = require('gulp-gh-pages');
 
 // Connect
 gulp.task('connect', function () {
-	$.connect.server({
-		root: ['slides'],
-		port: 9000,
-		livereload: {
-			port: 1337
-		}
-	});
+  $.connect.server({
+    root: ['slides'],
+    port: 9000,
+    livereload: {
+      port: 1337
+    }
+  });
 });
 
-// Scripts
-// gulp.task('scripts', function () {
-// 	return gulp.src('app/scripts/**/*.js')
-// 		.pipe($.jshint('.jshintrc'))
-// 		.pipe($.jshint.reporter('default'))
-// 		.pipe($.size());
-// });
 
 // CSS
 gulp.task('css', function () {
-	return gulp.src('slides/css/source/main.scss')
-		.pipe($.sass())
-		.pipe(gulp.dest('slides/css/'));
+  return gulp.src('slides/css/source/main.scss')
+    .pipe($.sass())
+    .pipe(gulp.dest('slides/css/'));
 })
 
 // Watch
 gulp.task('watch', ['connect'], function () {
-	// Watch for changes in `slides` folder
-	gulp.watch([
-		'slides/**/*.html',
-		'slides/js/**/*.js',
-		'slides/css/**/*.scss',
-		'slides/demos/**/*.js',
-		'slides/demos/demos.css'
-	], function(event) {
-		console.log(event.path)
-		return gulp.src(event.path)
-			.pipe($.connect.reload());
-	});
+  // Watch for changes in `slides` folder
+  gulp.watch([
+    'slides/**/*.html',
+    'slides/js/**/*.js',
+    'slides/css/**/*.scss',
+    'slides/demos/**/*.js',
+    'slides/demos/demos.css'
+  ], function(event) {
+    console.log(event.path)
+    return gulp.src(event.path)
+      .pipe($.connect.reload());
+  });
 
-	// Watch .js files
-	//gulp.watch('slides/js/**/*.js', ['js']);
-
-	// Watch scss files
-	gulp.watch('slides/css/**/*.scss', ['css']);
+  // Watch scss files
+  gulp.watch('slides/css/**/*.scss', ['css']);
 });
+
+// deploy to gh-pages
+gulp.task('deploy', function () {
+  return gulp.src('./slides/**/*')
+    .pipe(deploy('https://github.com/victormejia/d3-angularoc.git'));
+})
